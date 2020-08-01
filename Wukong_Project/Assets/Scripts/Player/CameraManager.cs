a2b01a805d499a82c0cb35f456ebae16d1656d47
Grizzly_BURR <benedettibrian1@gmail.com>
2020-08-01 08:40:37 +0200

    Almost finished with the lock on system.

--- Wukong_Project/Assets/Scripts/Player/CameraManager.cs
+++ Wukong_Project/Assets/Scripts/Player/CameraManager.cs
@@ -8,23 +8,9 @@ public class CameraManager : MonoBehaviour
 {
     private PlayerInputActions inputActions;
 
-    //float yaw;
-    //float pitch;
-    //float mouseSensitivity = 0.5f;
-    //public float distanceToTarget;
-    //public float rotationSmoothTime = 0.12f;
-    //public float movementSmoothTime = 10;
-
-    //public Transform targetLook;
     public CinemachineFreeLook mainCamera;
 
     Vector2 cameraInput;
-    //Vector2 pitchMinMax = new Vector2(-40, 80);
-
-    //Vector3 rotationSmoothVelocity;
-    //Vector3 currentRotation;
-    //public Vector3 offset;
-
 
     private void Awake()
     {
@@ -34,20 +20,6 @@ public class CameraManager : MonoBehaviour
         inputActions.PlayerControls.Camera.canceled += ctx => cameraInput = Vector2.zero;
     }
 
-    //private void FixedUpdate()
-    //{
-    //    yaw += cameraInput.x * mouseSensitivity;
-    //    pitch -= cameraInput.y * mouseSensitivity;
-    //    pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
-
-    //    currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
-
-    //    transform.eulerAngles = currentRotation;
-
-    //    Vector3 desiredPosition = targetLook.position + offset;
-    //    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, movementSmoothTime * Time.deltaTime);
-    //    transform.position = smoothedPosition;
-    //}
     private void LateUpdate()
     {
         mainCamera.m_XAxis.m_InputAxisValue = cameraInput.x * mainCamera.m_XAxis.m_MaxSpeed * Time.deltaTime;
---
+++ Wukong_Project/Assets/Scripts/Player/LockOnSystem.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LockOnSystem : MonoBehaviour
+{
+    PlayerInputActions inputActions;
+
+    public Camera mainCamera;
+
+    public CinemachineFreeLook mainVirtualCamera;
+    public CinemachineVirtualCamera lockOnCamera;
+    public CinemachineTargetGroup targetGroup;
+
+    bool isLockedOn;
+
+    int index;
+
+    public float lockOnRange;
+
+    string enemyTag = "Enemy";
+
+    Transform target;
+
+    [SerializeField] List<Transform> allEnemies = new List<Transform>();
+    [SerializeField] List<Transform> enemiesInRange = new List<Transform>();
+
+    GameObject[] enemiesFound;
+
+    public GameObject lockOnCursor;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = null;
+        lockOnCursor.SetActive(false);
+        isLockedOn = false;
+        index = 0;
+
+        FindAllEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputActions.PlayerControls.LockOn.triggered && !isLockedOn)
+        {
+            FindAllEnemies();
+            CheckForEnemiesInRange();
+            
+            if (enemiesInRange.Count > 0)
+            {
+                LockOn();
+            }
+        }
+        else if(inputActions.PlayerControls.LockOn.triggered && isLockedOn || enemiesInRange.Count < 1 || Vector3.Distance(this.gameObject.transform.position, target.position) > lockOnRange)
+        {
+            mainVirtualCamera.gameObject.SetActive(true);
+            lockOnCamera.gameObject.SetActive(false);
+            lockOnCursor.SetActive(false);
+            isLockedOn = false;
+            //problem is it is blendin and not immidiate
+            targetGroup.m_Targets[1].target = null;
+            target = null;
+            enemiesInRange.Clear();
+        }
+
+        if(inputActions.PlayerControls.SwitchTarget.triggered && isLockedOn)
+        {
+            CycleThroughEnemies();
+        }
+
+        if (isLockedOn)
+        {
+            target = enemiesInRange[index];
+            targetGroup.m_Targets[1].target = target;
+            lockOnCursor.transform.position = mainCamera.WorldToScreenPoint(target.position);
+        }
+    }
+
+    void LockOn()
+    {
+        isLockedOn = true;
+        lockOnCursor.SetActive(true);
+        lockOnCamera.gameObject.SetActive(true);
+        mainVirtualCamera.gameObject.SetActive(false);
+    }
+
+    void FindAllEnemies()
+    {
+        enemiesFound = GameObject.FindGameObjectsWithTag(enemyTag);
+        foreach (GameObject e in enemiesFound)
+        {
+            if (!allEnemies.Contains(e.transform))
+            {
+                allEnemies.Add(e.transform);
+            }
+        }
+    }
+
+    void CheckForEnemiesInRange()
+    {
+        if(allEnemies.Count > 0)
+        {
+            foreach (Transform e in allEnemies)
+            {
+                Vector3 ePos = mainCamera.WorldToViewportPoint(e.position);
+
+                if ((Vector3.Distance(this.gameObject.transform.position, e.position) <= lockOnRange) && !enemiesInRange.Contains(e))
+                {
+                    enemiesInRange.Add(e);
+                }
+                else
+                {
+                    enemiesInRange.Remove(e);
+                }
+            }
+        }        
+    }
+
+    void CycleThroughEnemies()
+    {
+        if (isLockedOn)
+        {
+            enemiesInRange.Clear();
+            CheckForEnemiesInRange();
+
+            if(enemiesInRange.Count > 0)
+            {
+                index++;
+                if (index > enemiesInRange.Count - 1)
+                {
+                    index = 0;
+                }
+            }            
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, lockOnRange);
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+}
