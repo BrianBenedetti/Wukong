6af51914f343d3db38a34c3970f3fb9aa1b86e23
Grizzly_BURR <benedettibrian1@gmail.com>
2020-08-06 10:38:48 +0200

    Fixed a Few Things and Built

--- Wukong_Project/Assets/Scripts/Enemies/Fast Enemy/FastEnemy_HurtState.cs
+++ Wukong_Project/Assets/Scripts/Enemies/Fast Enemy/FastEnemy_HurtState.cs
@@ -7,7 +7,7 @@ public class FastEnemy_HurtState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Random.Range(1, 3) == 1)
+        if (Random.Range(0, 2) == 0)
         {
             animator.SetTrigger("Dodge");
         }
